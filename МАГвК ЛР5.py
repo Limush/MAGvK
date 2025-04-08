80ae1294a426a553d2da529dd56ab82b49140d65
Limush <dzaikin999@gmail.com>
2025-04-08 15:30:22 +0300

    version 1.0

--- МАГвК ЛР5.py
+++ МАГвК ЛР5.py
@@ -1,22 +1,3 @@
-from prettytable import PrettyTable
-
-
-def inference(array, task):
-    numbers = [str(i) for i in range(0, len(array_x))]
-    table = PrettyTable()
-    table.field_names = [""] + [f"{num+1}" for num in range(len(numbers))]
-    if task == 'x':
-        table.add_row(['x'] + numbers)
-        table.add_row([f"x^3 + {a}x + {b} (mod {p})"] + array)
-    elif task == 'y':
-        table.add_row(["y"] + numbers)
-        table.add_row([f"    y^2 (mod {p})     "] + array)
-    table.header = False
-    for i in table.field_names:
-        table.align[i] = "c"
-    return table
-
-
 def poisk_sum(P, Q):
     try:
         k_sum = pow((Q[1] - P[1]) * pow((Q[0] - P[0]), -1, p), 1, p)
