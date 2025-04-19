1ffcb4f8fe380ad8839922d8acfa08e8d969d7b5
Limush <dzaikin999@gmail.com>
2025-04-19 10:51:21 +0300

    Full

--- МАГвК ЛР6.py
+++ МАГвК ЛР6.py
@@ -4,6 +4,16 @@ from math import sqrt
 from prettytable import PrettyTable
 
 
+def Input_P():
+    try:
+        P = (input("Введите координаты P через пробел -> ").split())
+        P = (int(P[0]), int(P[1]))
+        return P
+    except:
+        print(f"Введено не правильно")
+        return False
+
+
 def poisk_sum(P, Q):
     if P == 'O' and Q != 'O':
         return Q[0], Q[1]
@@ -32,8 +42,7 @@ def Equation(function, x):
     return eval(function.replace('x', str(x)))
 
 
-
-def find_xP(count):
+def find_xP(count, P):
     array_chet = [(0, 0)]
     while len(array_chet) != count + 1:
         if len(array_chet) == 1:
@@ -56,6 +65,7 @@ def find_xP(count):
                     break
     return array_chet
 
+
 a = int(input('Введите a -> '))
 b = int(input('Введите b -> '))
 p = int(input('Введите p -> '))
@@ -64,65 +74,47 @@ function = f"x**3+{a}*x+{b}"
 array_x, array_y = [], []
 for i in range(p):
     array_x.append(Equation(function, i) % p)
-    array_y.append((i*i) % p)
+    array_y.append((i * i) % p)
 
-array_per = ["O"]
+array_per, array_full = ["O"], ["O"]
 for x in range(len(array_x)):
     if array_x[x] in array_y:
         for y in range(len(array_y)):
             if array_x[x] == array_y[y]:
                 array_per.append(f"{x}, {y}")
-print(f"y^2 = x^3 + {a}x + {b} (mod {p})\n"
-      f"\tТочки ЭК:")
+                array_full.append((x, y))
+print(f"y^2 = x^3 + {a}x + {b} (mod {p})\n\tТочки ЭК:")
 print('(' + ');('.join(array_per[1:]) + ')' + f" и {array_per[0]}")
 
-P = (2, 4)
-try:
-    P = (input("Введите координаты P через пробел -> ").split())
-    P = (int(P[0]), int(P[1]))
-except:
-    print("Введено не правильно")
-
-count = 7
+P = Input_P()
 while True:
     task = input(f"1 - Поменять точку\n2 - Считать для текущих данных\n--->")
-    if task == '1':
-        try:
-            P = (input("Введите координаты P через пробел -> ").split())
-            P = (int(P[0]), int(P[1]))
-        except:
-            print("Введено не правильно")
-    else:
-        cheak = 0
-        for i in range(1, len(array_per)):
-            sim1, sim2 = array_per[i].split(", ")
-            if P[0] == int(sim1) and P[1] == int(sim2):
-                cheak += 1
-        if cheak == 1:
+    if task == '1' or P == False:
+        P = Input_P()
+    elif task == '2':
+        print(array_full)
+        print(P)
+        if P in array_full:
             print(f"\tПункт 1:")
             N1 = p + 1 + 2 * sqrt(p)
             m = math.ceil(sqrt(N1))
             print(f"N1 = {N1}\n"
                   f"m = {m}")
             print(f"\tПункт 2:")
-            P_t = find_xP(m)[1:]
+            P_t = find_xP(m, P)[1:]
             table = PrettyTable()
-            table.add_row(['t'] + [i for i in range(1, m+1)])
+            table.add_row(['t'] + [i for i in range(1, m + 1)])
             table.add_row(['P(t)'] + P_t)
             table.header = False
             print(table)
             print(f"\tПункт 3:")
-            if P_t[-1] != 'O':
-                Q = (P_t[-1][0], (P_t[-1][1] * (-1)) % p )
-            else:
-                Q = 'O'
+            Q = (P_t[-1][0], (P_t[-1][1] * (-1)) % p) if P_t[-1] != 'O' else 'O'
             print(f"Q = {Q}")
             print(f"\tПункт 4:")
             R = 'O'
             print(f"R = {R}")
             print(f"\tПункт 5:")
-            t = -1
-            i = 0
+            t, i = -1, 0
             for y in range(m):
                 if R in P_t:
                     t = P_t.index(R)
@@ -145,3 +137,5 @@ while True:
                 print(f"t не найдено\n")
         else:
             print("Это не точка ЭК")
+    else:
+        break
